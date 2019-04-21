ef4c8cfb841ca9193b92560c3aae619919bf5dbb
Robert <lenzrobert@users.noreply.github.com>
2019-04-21 13:57:08 +0200

    IValueConverter implemented (UvValueToStringConverter.cs) and added to view for the AirAndPollen section

---
+++ Wpf.Masterclass.AccuWeather/Converters/UvValueToStringConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wpf.Masterclass.AccuWeather.Converters
+{
+    public class UvValueToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                string uvValueString = value.ToString();
+                if (int.TryParse(uvValueString, out int uvValue))
+                {
+                    if (uvValue < 1)
+                    {
+                        return $"({uvValue}) - Low";
+                    }
+
+                    if (uvValue >= 2)
+                    {
+                        return $"({uvValue}) - Medium";
+                    }
+                    if (uvValue < 2)
+                    {
+                        return $"({uvValue}) - High";
+                    }
+
+                }
+            }
+
+            return "N/A";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
